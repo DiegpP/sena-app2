9f809c39d8000f3ff47471433bf4037a7fd72d15
DiegopP <darktro900@gmail.com>
2024-08-13 19:49:46 -0500

    base del proyecto

---
+++ src/main/java/com/spring/appSena/App_Sena/AppSenaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.appSena.App_Sena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSenaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/appSena/App_Sena/AppSenaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.appSena.App_Sena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSenaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
